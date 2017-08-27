ee51730b4d7c9a03af68089395be47689716549a
Michael Nguyen <tehtotalpwnage@gmail.com>
2017-08-27 12:15:00 -0700

    Add install commands for Artisan

---
+++ app/Console/Commands/Install.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Install extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'install:docker';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Installs the application if deployed using Docker.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('Beginning app installation process...');
+
+
+        /**
+         * This used to copy the .env file, but that would run into issues,
+         * since the instance that is running this install script doesn't have
+         * access to the new .env file quite yet.
+         */
+
+        $this->call('key:generate');
+
+        /**
+         * Don't use "migrate:refresh --seed" here, that command does not work
+         * when called in this manner.
+         */
+        $this->call('migrate');
+        $this->call('db:seed');
+        $this->call('install:oauth');
+        // $this->call('passport:install', [
+        //     'user' => 1, '--queue' => 'default'
+        // ]);
+        $this->info('Done! Remember to edit the .env file to set this project into production.');
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
+        Commands\Install::class,
         Commands\Oauth::class
     ];
 
