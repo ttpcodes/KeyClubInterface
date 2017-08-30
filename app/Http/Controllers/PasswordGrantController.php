ece1c284de05d6604a8ac21a170a0e29b5cb535c
Michael Nguyen <tehtotalpwnage@gmail.com>
2017-08-29 21:28:43 -0700

    Fix issues with password grant

--- app/Http/Controllers/PasswordGrantController.php
+++ app/Http/Controllers/PasswordGrantController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use GuzzleHttp;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\RequestException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
@@ -20,20 +22,34 @@ class PasswordGrantController extends Controller
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
             Log::info('Successfully verified user. Now initiating password grant...');
             $http = new GuzzleHttp\Client;
+            try {
+                $response = $http->post(env('APP_URL') . '/oauth/token', [
+                    'form_params' => [
+                        'grant_type' => 'password',
+                        'client_id' => env('PASSWORD_CLIENT_ID'),
+                        'client_secret' => env('PASSWORD_CLIENT_SECRET'),
+                        'username' => $request->email,
+                        'password' => $request->password,
+                        'scope' => '*'
+                    ],
+                ]);
 
-            $response = $http->post(env('APP_URL') . '/oauth/token', [
-                'form_params' => [
-                    'grant_type' => 'password',
-                    'client_id' => env('PASSWORD_CLIENT_ID'),
-                    'client_secret' => env('PASSWORD_CLIENT_SECRET'),
-                    'username' => $request->email,
-                    'password' => $request->password,
-                    'scope' => '*'
-                ],
-            ]);
-
-            return json_decode((string) $response->getBody(), true);
+                return json_decode((string) $response->getBody(), true);
+            } catch (ConnectException $e) {
+                return response()->json([
+                    'status' => 500,
+                    'error' => 'GuzzleHttp APP_URL improperly configured.'
+                ], 500);
+            } catch (RequestException $e) {
+                return response()->json([
+                    'status' => 500,
+                    'error' => 'PASSWORD_CLIENT improperly configured.'
+                ]);
+            }
         }
-
+        return response()->json([
+            'status' => 401,
+            'error' => 'Incorrect credentials.'
+        ], 401);
     }
 }
